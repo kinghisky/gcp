87eddcde27a724522770c88756a2bfc60eee3b64
moluanshanhe <1359493339@qq.com>
2020-01-26 16:52:53 +0800

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+until
+		echo "1.centos"
+		echo "2.ubuntu"
+		read input
+		test $input = 2
+		do
+			case $input in
+			1)sudo passwd
+sudo sed -i 's|PermitRootLogin no|PermitRootLogin yes|' /etc/ssh/sshd_config
+sudo sed -i 's|PasswordAuthentication no|PasswordAuthentication yes|' /etc/ssh/sshd_config
+sudo /etc/init.d/ssh restart;;
+			2);;
+      esac
+			done
