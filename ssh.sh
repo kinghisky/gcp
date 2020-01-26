2438929b30d02fc285c6d96a4aeb84250908ef21
moluanshanhe <1359493339@qq.com>
2020-01-26 17:13:49 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -10,6 +10,9 @@ until
 sudo sed -i 's|PermitRootLogin no|PermitRootLogin yes|' /etc/ssh/sshd_config
 sudo sed -i 's|PasswordAuthentication no|PasswordAuthentication yes|' /etc/ssh/sshd_config
 sudo /etc/init.d/ssh restart;;
-			2);;
+			2)sudo passwd
+sudo sed -i 's|PermitRootLogin prohibit-password|#PermitRootLogin prohibit-password|' /etc/ssh/sshd_config
+sudo sed -i 's|PasswordAuthentication no|PasswordAuthentication yes|' /etc/ssh/sshd_config
+sudo service ssh restart;;
       esac
 			done
