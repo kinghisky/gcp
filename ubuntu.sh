30bfa6e213cd2bfd0029221e84ec6965d13f9471
moluanshanhe <1359493339@qq.com>
2020-01-30 20:06:55 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,4 +2,4 @@
 sudo passwd
 sudo sed -i 's|PermitRootLogin prohibit-password|#PermitRootLogin prohibit-password|' /etc/ssh/sshd_config
 sudo sed -i 's|PasswordAuthentication no|PasswordAuthentication yes|' /etc/ssh/sshd_config
-sudo service ssh restart;;
+sudo /etc/init.d/ssh restart
